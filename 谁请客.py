251d3760d4339250da09ee557b55e852301953fb
Veralovescoding <90300839+Veralovescoding@users.noreply.github.com>
2021-09-09 15:15:21 +0800

    第一版，随机抽人
    随机抽人

---
+++ 谁请客.py
@@ -0,0 +1,13 @@
+print('今天谁负责买单？？')
+import random
+import time
+EBC_web=['Vera','栖铭','朱丽丝','范范']
+print('Loading.......')
+time.sleep(1)
+for x in range(len(EBC_web)-1):
+    cola = random.randint(0, (len(EBC_web))-1)
+    print('It is not '+EBC_web[cola]+'.....')
+    del EBC_web[cola]
+    time.sleep(1)
+
+print('It is '+EBC_web[0])
