f5cd4514e52cca091f130bb6e5dec87dbbf47eec
Veralovescoding <90300839+Veralovescoding@users.noreply.github.com>
2021-09-09 15:16:58 +0800

    Update 谁请客.py
    加了一个人

--- 谁请客.py
+++ 谁请客.py
@@ -1,7 +1,7 @@
 print('今天谁负责买单？？')
 import random
 import time
-EBC_web=['Vera','栖铭','朱丽丝','范范']
+EBC_web=['Vera','栖铭','朱丽丝','范范','Huahua']
 print('Loading.......')
 time.sleep(1)
 for x in range(len(EBC_web)-1):
